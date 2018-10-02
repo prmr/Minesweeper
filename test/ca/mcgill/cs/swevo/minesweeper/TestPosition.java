191cd0986e26f440f913bdd6ae0390f7cb4656be
prmr <prmr@gmx.com>
2018-10-02 14:22:52 +0200

    Add tests for position

--- test/ca/mcgill/cs/swevo/minesweeper/TestPosition.java
+++ test/ca/mcgill/cs/swevo/minesweeper/TestPosition.java
@@ -21,4 +21,26 @@ public class TestPosition
 		assertEquals(2, position.getRow());
 		assertEquals(7, position.getColumn());
 	}
+	
+	@Test
+	public void testHashCode()
+	{
+		assertEquals(961, new Position(0,0).hashCode());
+		assertEquals(1180, new Position(2,7).hashCode());
+		assertEquals(1030, new Position(7,2).hashCode());
+		assertEquals(1825, new Position(27,27).hashCode());
+	}
+	
+	@Test
+	public void testEquals()
+	{
+		Position p1 = new Position(2,7);
+		Position p2 = new Position(2,7);
+		Position p3 = new Position(7,2);
+		assertFalse(p1.equals(null));
+		assertTrue(p1.equals(p1));
+		assertTrue(p1.equals(p2));
+		assertTrue(p2.equals(p1));
+		assertFalse(p1.equals(p3));
+	}
 }
