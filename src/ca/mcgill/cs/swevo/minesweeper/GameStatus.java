6e19a8b9a597ec59b2d7690cb259e2ac84e0ac62
prmr <prmr@gmx.com>
2018-10-02 14:31:34 +0200

    Document GameStatus

--- src/ca/mcgill/cs/swevo/minesweeper/GameStatus.java
+++ src/ca/mcgill/cs/swevo/minesweeper/GameStatus.java
@@ -1,6 +1,25 @@
 package ca.mcgill.cs.swevo.minesweeper;
 
+/**
+ * Main abstract states for the game.
+ * 
+ * @author Martin P. Robillard
+ */
 public enum GameStatus
 {
-	IN_PLAY, WON, LOST
+	/**
+	 * One or more cells remain hidden.
+	 */
+	IN_PLAY, 
+	
+	/**
+	 * All the cells have been revealed, and the location of all mines
+	 * has been correctly marked.
+	 */
+	WON, 
+	
+	/**
+	 * At least one cell containing a mine has been revealed.
+	 */
+	LOST
 }
