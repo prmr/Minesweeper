712f64658715e8b9ded03f627c87155a06c9b111
prmr <prmr@gmx.com>
2018-10-02 14:29:18 +0200

    Document CellStatus

--- src/ca/mcgill/cs/swevo/minesweeper/CellStatus.java
+++ src/ca/mcgill/cs/swevo/minesweeper/CellStatus.java
@@ -1,6 +1,37 @@
 package ca.mcgill.cs.swevo.minesweeper;
 
+/**
+ * Represents the status of a cell during a game.
+ * 
+ * @author Martin P. Robillard
+ */
 public enum CellStatus
 {
-	MARKED, HIDDEN, MINE, CLEAR, PROXIMITY
+	/**
+	 * The cell has been marked by the player as hiding a mine.
+	 */
+	MARKED,
+	
+	
+	/**
+	 * The cell is hidden and not marked.
+	 */
+	HIDDEN, 
+	
+	/**
+	 * The cell contains a mine and the mine is revealed.
+	 */
+	MINE, 
+	
+	/**
+	 * The cell has been revealed, does not contain a mine, and
+	 * none of its neighbouring cells hides a mine.
+	 */
+	CLEAR, 
+	
+	/**
+	 * The cell has been revealed, does not contain a min,
+	 * and one or more of its neighbouring cells hides a mine.
+	 */
+	PROXIMITY
 }
